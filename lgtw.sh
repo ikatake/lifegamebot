ce94402cf630db0f49f39222ae5a4b68205f828d
detcoder <github@detcoder.org>
2016-07-02 01:13:05 +0900

    MOD lgtw.sh to Generate PNG image.
    stateの更新時にPNGでも画像を出力するように

--- lgtw.sh
+++ lgtw.sh
@@ -14,6 +14,7 @@ if [ -s ./status.txt ]; then #status.txtに内容があれば(初期化)、状�
   cp -p status.txt status`date '+%s'`.txt
 else #status.txtが空であれば、状態をつぶやく
   perl ./makeSVG.pl ./state.new > ./state.svg
+  perl ./makePNG.pl ./state.new
   perl ./saveLog.pl ./state.new ./state.svg
   perl ./trans.pl ./state.new trans.conf.pl > ./tweet.txt
 fi
